dbda2b9cf8698c63130a8e1a3eb363961b323a96
Gui <marinho.silva.guilherme@escola.pr.gov.br>
2025-11-27 11:27:08 +0000

    a

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { BottomTabs } from 'react-native-screens';
 
 
 export default function App() {
   return (
-    
+
     <View style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor="#1e697c" />
       <Text style={styles.welcomeText}>Bem vindo ao seu app!</Text>
@@ -11,20 +12,29 @@ export default function App() {
   );
 }
 
+export default funtion Botão(){
+  return(
+    <view>
+      <Text>Avançar</Text> 
+    </view>
+  )
+}
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#8ee8ffff', 
-    justifyContent: 'center', 
-    alignItems: 'center', 
-    paddingHorizontal: 30, 
+    backgroundColor: '#adecfcff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 30,
   },
- 
+
   welcomeText: {
-    fontSize: 24, 
+    fontSize: 24,
     paddingBottom: 850,
-    fontWeight: 'bold', 
-    color: '#000000ff', 
-    textAlign: 'center', 
+    fontWeight: 'bold',
+    color: '#000000ff',
+    textAlign: 'center',
   },
 });
