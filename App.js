dcd06c034820e0675c9410c0057929daae26174c
Gui <marinho.silva.guilherme@escola.pr.gov.br>
2025-11-27 09:21:27 +0000

    a

--- App.js
+++ App.js
@@ -1,39 +1,20 @@
-// App.js
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import TelaInicial from './TelaInicial'; // Importa a primeira tela
-import TelaSecundaria from './TelaSecundaria'; // Importa a segunda tela
-
-// Cria o Navegador de Pilha
-const Stack = createNativeStackNavigator();
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    // O NavigationContainer gerencia o estado de navegação
-    <NavigationContainer>
-      {/* O Stack.Navigator define a ordem das telas */}
-      <Stack.Navigator 
-        initialRouteName="Inicial" 
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#f4511e', // Cor do cabeçalho
-          },
-          headerTintColor: '#fff', // Cor do texto do cabeçalho
-          headerTitleAlign: 'center', // Centraliza o título em todas as telas
-        }}
-      >
-        <Stack.Screen 
-          name="Inicial" 
-          component={TelaInicial} 
-          options={{ title: 'Tela Inicial' }} // Título do cabeçalho
-        />
-        <Stack.Screen 
-          name="Secundaria" 
-          component={TelaSecundaria} 
-          options={{ title: 'Detalhes' }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Bem vindo ao seu app</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
