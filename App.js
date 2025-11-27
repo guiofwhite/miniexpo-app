a6dee5a9bb98cad4067731bab0678d3fc956d8b3
Gui <marinho.silva.guilherme@escola.pr.gov.br>
2025-11-27 09:53:09 +0000

    3

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TouchableOpacity, Alert  } from 'react-native';
 
 
 export default function App() {
+
+  const lidarComClique = () => {
+        Alert.alert("Ação do Botão", `Você clicou no perfil de ${nome}!`);
+    };
   return (
     
     <View style={styles.container}>
       <StatusBar barStyle="light-content" backgroundColor="#1e697c" />
       <Text style={styles.welcomeText}>Bem vindo ao seu app!</Text>
+
+      <TouchableOpacity 
+                style={estilos.botao} 
+                onPress={lidarComClique}
+            >
+                <Text style={estilos.textoBotao}>Ver Perfil</Text>
+            </TouchableOpacity>
     </View>
   );
 }
@@ -27,4 +38,23 @@ const styles = StyleSheet.create({
     color: '#000000ff', 
     textAlign: 'center', 
   },
+
+   botao: {
+        marginTop: 15,
+        backgroundColor: '#5b82a8',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 5,
+        elevation: 5, 
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+    },
+    
+    textoBotao: {
+        color: '#ffffff',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
 });
